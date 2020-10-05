867ad8aaa2068cbc683da8a61a704504a8746407
yeqianfeng <2690380103@qq.com>
2020-10-05 19:21:20 +0800

    my-learning

---
+++ src/com/qfye/java/week1/Factorial.java
@@ -0,0 +1,19 @@
+package com.qfye.java.week1;
+
+/**
+ * @ClassName Factorial
+ * @Description TODO
+ * @Author QIAN-feng
+ * @Date 2020/10/5
+ **/
+public class Factorial {
+    public static void main(String[] args) {
+        //定义一个整型变量
+        int n = 5;
+        int result = 1;
+        for(int i = 1; i<= n; i++){
+            result = result * i;
+        }
+System.out.println(n + "的阶乘是：" + result);
+    }
+}
